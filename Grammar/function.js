1c1e4b55bda4d321e6e13842701370ed9fdc1fae
dana0221 <dana8478@naver.com>
2022-03-18 13:58:39 +0900

    함수연습

---
+++ Grammar/function.js
@@ -0,0 +1,12 @@
+function 문자출력함수(문자){
+    console.log(문자)
+}
+문자출력함수('mirim')
+
+// printChar = 문자출력함수
+// printChar('b')
+
+printChar = function(문자){
+    console.log(문자)
+}
+printChar('청소를 열심히 합시다')
