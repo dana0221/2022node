0cc058d3b981064d8901e8607876861b9bf9f819
dana0221 <dana8478@naver.com>
2022-04-08 14:35:58 +0900

    web2-31 글생성 UI

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function templateHTML(title, list, body){
                 <h1><a href="/">WEB</a></h1>
                 ${list}
                 <h2>${title}</h2>
+                <a href="/create">create</a>
                 <p>${body}</p>
             </body>
         </html>
@@ -48,7 +49,7 @@ const app = http.createServer(function (request, response) {
 
                 response.writeHead(200)
                 response.end(template)
-            });
+            })
 
 
         } else {
@@ -61,8 +62,30 @@ const app = http.createServer(function (request, response) {
                     response.writeHead(200)
                     response.end(template)
                 })
-            });
+            })
         }
+    } else if(pathname === '/create'){
+        fs.readdir('data/', function(err, data){
+            const title = 'Web - create'
+            const list = templateList(data)
+            const template = templateHTML(title, list, `
+                <form action = "create_process" method = "post">
+                    <p>
+                        <input type="text" name="title" placeholder="title"/>
+                    </p>
+                    <p>
+                        <textarea name="description" placeholder="description"></textarea>
+                    </p>
+                    <p>
+                        <input type="submit"/>
+                    </p>
+                </form>
+            `)
+
+            response.writeHead(200)
+            response.end(template)
+        })
+
     } else {
         response.writeHead(404)
         response.end('Not found')
