8b5ad3c4168eb4be42788aff08f4b93ce5afcd8d
dana0221 <dana8478@naver.com>
2022-06-13 14:24:02 +0900

    DB 연결

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+require('dotenv/config')
+
 const app = express()
-const PORT = process.env.PORT || 3333
+mongoose.connect(process.env.DB_CONNECTION , {useNewUrlParser:true, useUnifiedTopology:true})
+
+const db = mongoose.connection
+db.on('error', function(err){
+    console.log(err)
+})
+
+db.once('open', function(){
+    console.log('DB 연결')
+})
 
 // morgan HTTP request 기록해주는 미들웨어
 app.use(morgan('dev'))
 
+const PORT = process.env.PORT || 3333
 app.listen(PORT)
