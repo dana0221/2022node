3049e89638d452ece7da1b246dd83e5d4cc3c5f1
dana0221 <dana8478@naver.com>
2022-06-17 14:39:56 +0900

    update, delete 구현

---
+++ routes/employeeRoute.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const EmployeeController = require('../controllers/employeeController')
+
+router.get('/showAll', EmployeeController.showAll)
+router.get('/show', EmployeeController.show)
+router.get('/insert', EmployeeController.insert)
+router.put('/update', EmployeeController.update)
+router.delete('/delete', EmployeeController.destroy)
+
+module.exports = router
